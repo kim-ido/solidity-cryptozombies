20cf349845d55bf976f7756e0c4db5ca1f3d8fff
kim-ido <kimido987@gmail.com>
2023-05-29 18:59:44 +0900

    Add lesson-1, chapter-1

---
+++ lesson-1/chapter-2/Contract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// ^0.6.8 버전 후부터 SPDX 라이센스를 명시해줘야 하는 듯
+// https://spdx.org/licenses/ 여기서 기타 라이센스들을 확인 가능하다
+pragma solidity ^0.8.0; // 솔리디티 버전 선언
+
+contract ZombieFactory { // 컨트랙트 선언, Java의 class같은 느낌일까
+
+}
